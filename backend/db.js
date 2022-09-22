7c5b216eb398cdf010770adb9eb7af8f6955cd48
Jinzxc <jincz@bu.edu>
2022-09-21 22:04:06 -0400

    Add Atlas connection

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
+const LOCAL_URL = "mongodb://localhost/taskDB"
+const CLOUD_URL = "mongodb+srv://Testing:[Password]@h4itest.dbcauap.mongodb.net/taskDB"
+
 module.exports = async () => {
   try {
     const connectionParams = {
       useNewUrlParser: true,
     };
     await mongoose.connect(
-      "mongodb://localhost/taskdb", // Local DB for testing, could also switch to cloud based db (Atlas)
+      LOCAL_URL,
       connectionParams,
     );
     console.log("Connected to MongoDB");
